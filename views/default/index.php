5864386df7702fd81c97e4fbf40a7c4144c65b4c
Vikas Pandey <svikas.pandey@gmail.com>
2020-02-27 15:03:11 +0530

    update template

--- views/default/index.php
+++ views/default/index.php
@@ -5,7 +5,7 @@ use yii\helpers\Html;
         <div class="row">
             <div class="col-md-6 col-md-offset-3 _amd">
                 <h1>2 step Authentication</h1>
-                <p class="_ap">A Google Authenticator</p>
+                <p class="_ap">Enter Your Google Authenticator Code</p>
                 <hr>
                 <?= Html::beginForm(['/authenticator/default/check'], 'POST'); ?>
                     <div class="_aform">
--- views/default/scan.php
+++ views/default/scan.php
@@ -5,7 +5,7 @@ use yii\helpers\Html;
         <div class="row">
             <div class="col-md-6 col-md-offset-3 _amd">
                 <h1>2 step Authentication</h1>
-                <p class="_ap">A Google Authenticator</p>
+                <p class="_ap">Identify yourself by scannning the QR code with Google Authenticator app</p>
                 <hr>
                 <?= Html::beginForm(['/authenticator/default/scan'], 'POST'); ?>
                     <div class="_aform">
@@ -23,6 +23,7 @@ use yii\helpers\Html;
                             
                             <img class="img-fluid" src="<?php echo $qrCodeUrl ?>" alt="Verify this Google Authenticator"><br><br>
                         <?php } ?>
+                            <p class="_ap">Enter Your Google Authenticator Code</p>
 
                             <input type="text" class="form-control" name="code" placeholder="******"><br> <br>    
                             <button type="submit" class="btn btn-md btn-primary">Verify</button>
